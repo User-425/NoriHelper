d163a08761114d62467c8e267de4a6e697cc05f1
user-425 <user425.itsme@gmail.com>
2025-01-05 20:39:08 +0700

    fix: Keywords not showing in the response

--- keywordHandler.js
+++ keywordHandler.js
@@ -5,7 +5,9 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8'));
+const config = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "config.json"), "utf8")
+);
 
 const keywords = JSON.parse(
   fs.readFileSync(path.join(__dirname, "keywords.json"), "utf8")
@@ -39,15 +41,19 @@ export function handleKeyword(message, matches) {
     console.log(
       `\x1b[33m[Keyword Detected]\x1b[0m Category: ${category}, Word: ${word}`
     );
-    if (category === "lupis") {
-      message.reply('<@1120000868335497226> Look, there is ', word);
-    }
-    if (category === "valk") {
-      message.reply("\:)");
-      message.reply('<@331997722305626122> Master, You may want to check this out');
-    }
-    if (category === "cosmic") {
-      message.reply('<@855862887753842719> Look, there is ', word);
+    if (message.author.id === config.targetBotId) {
+      if (category === "lupis") {
+        message.reply(`<@1120000868335497226> Look, there is ${word}`);
+      }
+      if (category === "valk") {
+        message.reply(":)");
+        message.reply(
+          "<@331997722305626122> Master, You may want to check this out"
+        );
+      }
+      if (category === "cosmic") {
+        message.reply(`<@855862887753842719> Look, there is ${word}`);
+      }
     }
     if (category === "me" && message.author.id === config.ownerId) {
       message.reply(customMessage);
